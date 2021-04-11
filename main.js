c791f1b99f5dd631ef7b959af6424ea3f58001fd
Arnav-Rawat <77447759+Arnav-Rawat@users.noreply.github.com>
2021-04-11 17:21:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,87 +2,90 @@ canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext("2d");
 
 car1_width = 120;
-car1_height= 70;
-car1_image= "car1.png";
-car1_x= 10;
+car1_height = 70;
+car1_image = "car1.png";
+car1_x = 10;
 car1_y = 10;
 
 car2_width = 120;
-car2_height= 70;
-car2_image= "car2.png";
-car2_x= 10;
+car2_height = 70;
+car2_image = "car2.png";
+car2_x = 10;
 car2_y = 100;
 
-background_image= racing image.jpg;
+background_image = "racingimage.jpg";
 
-function add(){
-   background_imgTag = new Image();
-   background_imgTag.onload = uploadBackground;
-   background_imgTag.src = background_image;
+function add() {
+	background_imgTag = new Image(); //defining a variable with a new image
+	background_imgTag.onload = uploadBackground; // setting a function, onloading this variable
+	background_imgTag.src = background_image;   // load image
 
-   car1_imageTag = new Image();
-   car1_imgTag.onload = uploadcar1;
-   car1_imgTag.src = car1_image;
+	car1_imgTag = new Image(); //defining a variable with a new image
+	car1_imgTag.onload = uploadcar1; // setting a function, onloading this variable
+	car1_imgTag.src = car1_image;   // load image
 
-   car2_imageTag = new Image();
-   car2_imgTag.onload = uploadcar2;
-   car2_imgTag.src = car2_image;
+	car2_imgTag = new Image(); //defining a variable with a new image
+	car2_imgTag.onload = uploadcar2; // setting a function, onloading this variable
+	car2_imgTag.src = car2_image;   // load image
 }
 
 function uploadBackground() {
-    ctx.drawImage(background_imgTag, 0,0, canvas.width, canvas.height);
+	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
 }
 
 function uploadcar1() {
-    ctx.drawImage(car1_imgTag, car1_x, cae1_y, car1_width, car1_height);
+	ctx.drawImage(car1_imgTag, car1_x, car1_y, car1_width, car1_height);
 }
+function uploadcar2() {
+	ctx.drawImage(car2_imgTag, car2_x, car2_y, car2_width, car2_height);
+}
+
 
 window.addEventListener("keydown", my_keydown);
 
-function my_keydown(e) {
-    if (keyPressed == '38'){
-     car1_up();
-     console.log ("Up arrow key");
-    }
-
-    if (keyPressed == '40'){
-        car1_down();
-        console.log ("Down arrow key");
-       }
-
-    if (keyPressed == '37'){
-        car1_left();
-        console.log ("Left arrow key");
-       }
-
-       if (keyPressed == '39'){
-        car1_right();
-        console.log ("Right arrow key");
-       }
-    
-       if (keyPressed == '37'){
-        car1_left();
-        console.log ("Left arrow key");
-       }
-
-       if (keyPressed == '87'){
-        car2_up();
-        console.log ("W key");
-       }
-
-       if (keyPressed == '65'){
-        car2_left();
-        console.log ("A key");
-       }
-
-       if (keyPressed == '83'){
-            car2_down();
-            console.log ("S key");
-        }
-       
-       if (keyPressed == '68'){
-           car2_right();
-           console.log ("D key");
-       }
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+		if(keyPressed == '38')
+		{
+			car1_up();
+			console.log("up arrow key");
+		}
+		if(keyPressed == '40')
+		{
+			car1_down();
+			console.log("down arrow key");
+		}
+		if(keyPressed == '37')
+		{
+			car1_left();
+			console.log("left arrow key");
+		}
+		if(keyPressed == '39')
+		{
+			car1_right();
+			console.log("right arrow key");
+		}
+		if(keyPressed == '87')
+		{
+			car2_up();
+			console.log("key w");
+		}
+		if(keyPressed == '83')
+		{
+			car2_down();
+			console.log("key s");
+		}
+		if(keyPressed == '65')
+		{
+			car2_left();
+			console.log("key a");
+		}
+		if(keyPressed == '68')
+		{
+			car2_right();
+			console.log("key d");
+		}
 }
 
